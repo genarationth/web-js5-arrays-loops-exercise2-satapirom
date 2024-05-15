74f2187042056e2d99a3fa88008041f2d6a523e4
Satapirom Khawatthu. <Satapirom.12@gmail.com>
2024-05-15 15:04:13 +0700

    arrays loop excrcise 2

---
+++ script.js
@@ -0,0 +1,12 @@
+//1
+const ordarName =["Sofia", "David", "Juan"]
+console.log(ordarName);
+
+//2
+ordarName.push("Sara", "Augustin");
+ordarName.shift(1)
+console.log(ordarName);
+//3
+ordarName.splice(1,1, "Renata.");
+ordarName.push("Elena");
+console.log(ordarName);
